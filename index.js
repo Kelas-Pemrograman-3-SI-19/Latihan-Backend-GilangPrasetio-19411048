cc2d0f3c4efba41927dd57ff40265d3d860d5135
Gilang Prasetio <80299588+gilangpras@users.noreply.github.com>
2021-05-07 17:19:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({
 //     console.log(req.body)
 // })
 
-app.use('/kegiatan', require('../routes/kegiatan'))
+app.use('/kegiatan', require('./routes/kegiatan'))
 
 app.listen(3000, () => {
     console.log('server started')
